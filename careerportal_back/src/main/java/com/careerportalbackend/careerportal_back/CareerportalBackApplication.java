ecf6b1d4c577964fcec6d3107b89fa6ef404778f
kshitijrat <kshitijratnawat@gmail.com>
2025-01-24 22:29:19 +0530

    Add Backend

---
+++ careerportal_back/src/main/java/com/careerportalbackend/careerportal_back/CareerportalBackApplication.java
@@ -0,0 +1,14 @@
+package com.careerportalbackend.careerportal_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CareerportalBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CareerportalBackApplication.class, args);
+		System.out.println("\nRun Successful*************\n");
+	}
+
+}
---
+++ careerportal_back/src/test/java/com/careerportalbackend/careerportal_back/CareerportalBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.careerportalbackend.careerportal_back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CareerportalBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
